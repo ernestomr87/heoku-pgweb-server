230a36191ad7dfe763dfdf4bbbdb6ab34c11bd66
Ernesto Miguel Rodriguez Rodriguez <ernestomr87@gmail.com>
2019-04-20 20:50:09 -0400

    Update

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,89 +1,50 @@
 var express = require('express');
 var router = express.Router();
 
-const verifySignUp = require('./verifySignUp');
-const authJwt = require('./verifyJwtToken');
+const checkDuplicateUserNameOrEmail = require('./verifySignUp').checkDuplicateUserNameOrEmail;
+const verifyToken = require('./verifyJwtToken').verifyToken;
 
 var controllers = require('../controllers');
 var mailer = require('./../util/mailer');
 
-var process = controllers.process;
 var auth = controllers.auth;
-var users = controllers.users;
+var process = controllers.process;
 var typeOfPermits = controllers.typeOfPermits;
+var users = controllers.users;
 
 //  REST-API
 
 // EXTERNAL PANGEAMT
 router.get('/api/engines', process.getExternalEngines);
-router.post('/api/process_file', process.sendFileToExternalProcess);
 router.post('/api/notification', process.notification);
+router.post('/api/process_file', process.sendFileToExternalProcess);
 
 //AUTH
-router.post(
-  '/api/auth/signup',
-  [verifySignUp.checkDuplicateUserNameOrEmail],
-  auth.signup
-);
 router.post('/api/auth/signin', auth.signin);
+router.post('/api/auth/signup', [checkDuplicateUserNameOrEmail], auth.signup);
 
 //TypeOfProcesses
-router.get(
-  '/api/typeOfPermits/list',
-  [authJwt.verifyToken],
-  typeOfPermits.list
-);
-router.post('/api/typeOfPermits/add', [authJwt.verifyToken], typeOfPermits.add);
-router.put(
-  '/api/typeOfPermits/edit',
-  [authJwt.verifyToken],
-  typeOfPermits.edit
-);
-router.put(
-  '/api/typeOfPermits/remove',
-  [authJwt.verifyToken],
-  typeOfPermits.remove
-);
-router.put(
-  '/api/typeOfPermits/default',
-  [authJwt.verifyToken],
-  typeOfPermits.default
-);
-router.get(
-  '/api/typeOfPermits/engines',
-  [authJwt.verifyToken],
-  process.getExternalEngines
-);
+router.get('/api/typeOfPermits/engines', [verifyToken],process.getExternalEngines);
+router.get('/api/typeOfPermits/list', [verifyToken], typeOfPermits.list);
+router.post('/api/typeOfPermits/add', [verifyToken], typeOfPermits.add);
+router.put('/api/typeOfPermits/default', [verifyToken], typeOfPermits.default);
+router.put('/api/typeOfPermits/edit', [verifyToken], typeOfPermits.edit);
+router.put('/api/typeOfPermits/remove', [verifyToken], typeOfPermits.remove);
 
 //Process
-router.get('/api/process/user', [authJwt.verifyToken], auth.userContent);
 router.get('/api/process/:uuid', process.getProcessByUuid);
-router.post(
-  '/api/process/processFile',
-  [authJwt.verifyToken],
-  process.processFile
-);
-router.post('/api/process/quoteFile', [authJwt.verifyToken], process.quoteFile);
-router.post(
-  '/api/process/processFileAfterQuoteFile',
-  [authJwt.verifyToken],
-  process.processFileAfterQuoteFile
-);
-router.get('/api/process/process', [authJwt.verifyToken], process.getProcess);
-router.get('/api/process/process', [authJwt.verifyToken], process.getProcess);
+router.get('/api/process/process', [verifyToken], process.getProcess);
+router.get('/api/process/user', [verifyToken], auth.userContent);
+router.post('/api/process/processFile', [verifyToken], process.processFile);
+router.post('/api/process/processFileAfterQuoteFile', [verifyToken],process.processFileAfterQuoteFile);
+router.post('/api/process/quoteFile', [verifyToken], process.quoteFile);
 
 //USERS
-router.get('/api/users/listAll', [authJwt.verifyToken], users.listAll);
-router.get('/api/users/get/:id', [authJwt.verifyToken], users.get);
-router.post(
-  '/api/users/create',
-  [verifySignUp.checkDuplicateUserNameOrEmail],
-  [authJwt.verifyToken],
-  users.create
-);
-router.put('/api/users/remove', [authJwt.verifyToken], users.remove);
-router.put('/api/users/update', [authJwt.verifyToken], users.update);
-
 router.get('/api/email', mailer.main);
+router.get('/api/users/get/:id', [verifyToken], users.get);
+router.get('/api/users/listAll', [verifyToken], users.listAll);
+router.post('/api/users/create', [checkDuplicateUserNameOrEmail],[verifyToken],users.create);
+router.put('/api/users/remove', [verifyToken], users.remove);
+router.put('/api/users/update', [verifyToken], users.update);
 
 module.exports = router;
