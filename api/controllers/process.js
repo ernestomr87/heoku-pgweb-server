46829904fc2e7cc8cf12bd6cee7ee89ade5aca46
Ernesto M Rodriguez <ernestomr87@gmail.com>
2019-11-18 09:12:23 -0500

    no message

--- api/controllers/process.js
+++ api/controllers/process.js
@@ -757,7 +757,8 @@ module.exports = {
   quoteFile: async (req, res) => {
     try {
       console.log("\x1b[32m", "**************quoteFile*****************");
-      console.log(JSON.stringify(req.body));
+      console.log(JSON.stringify(req.body.process));
+      console.log(JSON.stringify(req.body.engine));
       console.log("\x1b[32m", "*******************************");
       const username = req.userEmail;
       const processId = req.body.process.id;
